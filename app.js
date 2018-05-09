abba9d9e70b59b87e11c1e081405ebab797cd19f
Jon Friskics <jonfriskics@gmail.com>
2018-05-09 13:04:25 -0400

    add task 1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/users', (req, res, next) => res.json({ users: ['Ray', 'Bob', 'Joe'] }))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 module.exports = app;
