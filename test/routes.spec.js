3282ae07269651e8906997bac922724ac194ddd4
Jon Friskics <jonfriskics@gmail.com>
2018-05-09 12:52:27 -0400

    tweaks for live test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,14 +5,14 @@ chai.use(require('chai-http'))
 const app = require('../app')
 
 describe('Routes', () => {
-  it('should return all users', function() {
+  it('should return all users @routes-return-all-users', function() {
     return chai.request(app)
       .get('/users')
       .then(function(res) {
         expect(res).to.have.status(200)
         expect(res).to.be.json
         expect(res.body).to.be.an('object')
-        expect(res.body.users).to.be.an('array').that.includes('Bill')
+        expect(res.body.users).to.be.an('array')
       })
   })
 })
